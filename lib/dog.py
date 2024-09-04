c197a6d9dc6cea92f477789af817497d723c84a4
baimet <isaacbaimetyatich@gmail.com>
2024-09-04 20:48:34 +0300

    Attributes and Properties

--- lib/dog.py
+++ lib/dog.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-APPROVED_BREEDS = [
+
+
+class Dog:
+
+    APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
     "Corgi",
@@ -10,6 +14,41 @@ APPROVED_BREEDS = [
     "Pug",
     "Pointer"
 ]
+    
+    def __init__(self, name="Dog", breed="Mastiff"):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            return  # Exit the constructor if the name is invalid
 
-class Dog:
-    pass
+        if breed in self.APPROVED_BREEDS:
+            print(f"Setting breed to {breed}.")
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            print(f"Setting name to {name}.")
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, breed):
+        if breed in self.APPROVED_BREEDS:
+            print(f"Setting breed to {breed}.")
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
+
--- lib/person.py
+++ lib/person.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-APPROVED_JOBS = [
+class Person:
+    APPROVED_JOBS = [
     "Admin",
     "Customer Service",
     "Human Resources",
@@ -15,5 +16,40 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
-class Person:
-    pass
+    def __init__(self, name="Person", job="Admin"):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()  # Convert to title case
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            return  # Exit the constructor if the name is invalid
+
+        if job in self.APPROVED_JOBS:
+            print(f"Setting job to {job}.")
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            print(f"Setting name to {name.title()}.")
+            self._name = name.title()  # Convert to title case
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    def get_job(self):
+        return self._job
+
+    def set_job(self, job):
+        if job in self.APPROVED_JOBS:
+            print(f"Setting job to {job}.")
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    job = property(get_job, set_job)
+
